3f2215d01fc3bd164a83d46575af3dd34b9308ec
Carl Cederborg <cederborgc@spu.edu>
2019-05-04 19:08:17 -0700

    host and port edits

--- app2.js
+++ app2.js
@@ -15,7 +15,7 @@ const server = http.createServer((request, response) => {
         '               <meta http-equiv="X-UA-Compatible" content="IE=edge"> \n' +
         '               <meta name="viewport" content="width=device-width, initial-scale=1"> \n' +
         '               <meta name="description" content="Home Page"> \n' +
-        '               <meta name="author" content="Carl Cederborg"> \n' +
+        '               <meta name="author" content="Carlos Arias"> \n' +
         '               <title>Netcentric Computing Home Page</title> \n' +
         '               <!-- Bootstrap core CSS --> \n' +
 		'				<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">\n' +
@@ -36,7 +36,6 @@ const server = http.createServer((request, response) => {
         '                                       <th scope="col">First Name</th> \n' +
         '                                       <th scope="col">Last Name</th> \n' +
         '                                       <th scope="col">Phone</th> \n' +
-        '                                       <th scope="col">Gender</th> \n' +
         '                               </tr> \n' +
         '                       </thead> \n' +
         '                       <tbody> \n'
@@ -45,10 +44,10 @@ const server = http.createServer((request, response) => {
 	        for (var f in friends[key])
 	            response.write(
 	                '                               <tr> \n' +
-	                '                                       <td>' + friends[key][f]["firstName"] + '</td> \n' +
+	                '                                       <td>' + friends[boys][key][f]["firstName"] + '</td> \n' +
 	                '                                       <td>' + friends[key][f]["lastName"] + '</td> \n' +
 	                '                                       <td>' + friends[key][f]["phone"] + '</td> \n' +
-                  '                                       <td>' + friends[key][f]["gender"] + '</td> \n' +
+
 	                '                               </tr> \n'
 	            );
 
